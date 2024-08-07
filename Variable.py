a7fb88e471dd4a70eac8fcd6ae95a04d0f078d33
pipin <Aleksejsoronov3@gmail.com>
2024-08-07 21:58:18 +0300

    Add files via upload

---
+++ Variable.py
@@ -0,0 +1,6 @@
+name = "Курс:Python,"
+task_count = 12
+hours_spent = 1.5
+Average_execution_time = 0.125
+print(name, "всего задач", str(task_count) + "," + " "+'затрачено часов',str(hours_spent)+','+" "'среднее время выполнения',
+     str(Average_execution_time) +'.')
